58ac484e69c39a9ce431d44103cba82eb811730d
Raahi Shah <raahishahonline@yahoo.com>
2018-04-15 00:11:23 +0530

    Mongoose Middleware

--- src/user.js
+++ src/user.js
@@ -20,8 +20,15 @@ const UserSchema = new Schema({
 });
 
 UserSchema.virtual('postCount').get(function(){
-    return this.posts.length;
+    return this.posts.length;    
 })
+
+UserSchema.pre('remove', function(next){
+const BlogPost = mongoose.model('blogPost');
+
+BlogPost.remove({_id: {$in: this.blogPosts}})
+    .then(()=>next());
+});
 
 const User = mongoose.model('user', UserSchema);
 
